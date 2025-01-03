ee2c0b631d535589212b176b6501f93cb2659313
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2025-01-03 23:06:11 +0200

    Added the first 3 sections
    Added the 3 first ones , the navbar , their images , the public & src folder

---
+++ src/components/ThirdSection.vue
@@ -0,0 +1,12 @@
+<template>
+    <section id="third-section">
+        <div id="third-inner-contents">
+            <h2>iPhone 16 Pro</h2>
+            <p>Hello, Apple Intelligence.</p>
+            <div>
+                <button id="learn-more">Learn more</button>
+                <button id="buy">Buy</button>
+            </div>
+        </div>
+    </section>
+</template>
